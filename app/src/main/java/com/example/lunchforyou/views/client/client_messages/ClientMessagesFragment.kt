45fda123a9567b34ee13e87d75ceaf2043268aa1
harkor15 <kibkib1998@gmail.com>
2021-06-18 15:13:54 +0200

    Settings, subscriptions and other layouts

--- app/src/main/java/com/example/lunchforyou/views/client/client_messages/ClientMessagesFragment.kt
+++ app/src/main/java/com/example/lunchforyou/views/client/client_messages/ClientMessagesFragment.kt
@@ -35,7 +35,7 @@ class ClientMessagesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_blank2, container, false)
+        return inflater.inflate(R.layout.fragment_client_messages, container, false)
     }
 
     companion object {
