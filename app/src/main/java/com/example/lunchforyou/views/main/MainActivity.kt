8dfcf7dcfff3177748a7397bf350e7c90cb6ab08
krzysztof_bigos <krzbigos@gmail.com>
2021-10-25 21:04:40 +0200

    Database test

--- app/src/main/java/com/example/lunchforyou/views/main/MainActivity.kt
+++ app/src/main/java/com/example/lunchforyou/views/main/MainActivity.kt
@@ -5,17 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.lunchforyou.R
-import com.example.lunchforyou.database.DatabaseManager
-import com.example.lunchforyou.database.IDeleteClient
+import com.example.lunchforyou.database.*
+import com.parse.ParseObject
 
 
-class MainActivity : AppCompatActivity(), IDeleteClient {
+class MainActivity : AppCompatActivity(), DatabaseResponseInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Client2.Create("aaaaa","Name","Surname", "Address",null, this)
+        //Client2.Read("aaaaa",this)
 
-        DatabaseManager.deleteClient("aaaaaaaaaa",this )
+
+        //DatabaseManager.deleteClient("aaaaaaaaaa",null )
         //DatabaseManager.getClient("aaaaaaaaaa")
        /* val firstObject = ParseObject("FirstClass")
         firstObject.put("message","Hey ! First message from android. Parse is now connected")
@@ -26,15 +29,18 @@ class MainActivity : AppCompatActivity(), IDeleteClient {
                 Log.d("MainActivity","Object saved.")
             }
         }*/
-
-
     }
 
-    override fun userDeleted() {
-        Log.d(TAG, "Success")
+    override fun success(table: String?, operation: DatabaseOperationType?) {
+        Log.d(TAG,"Success")
     }
 
-    override fun error(e: String) {
-        Log.d(TAG, e)
+    override fun error(e: String, table: String?, operation: DatabaseOperationType?) {
+        Log.d(TAG, "ERROR $e")
+    }
+
+    override fun readed(parseObject: ParseObject) {
+        val client = Client2(parseObject)
+        Log.d(TAG,"Readed: ${client.token} name: ${client.name} ")
     }
 }
